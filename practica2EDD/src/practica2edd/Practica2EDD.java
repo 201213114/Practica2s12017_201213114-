aed603ad51a7f931801ded79191fce4782565f17
201213114 <gorgojo.mario@gmail.com>
2017-02-26 13:34:57 -0600

    commit de prueba Pra2

---
+++ practica2EDD/src/practica2edd/Practica2EDD.java
@@ -0,0 +1,13 @@
+package practica2edd;
+
+
+public class Practica2EDD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
